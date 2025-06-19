5d83aa81de373a9904a4b7d09ae2da82375bcbdb
Overlord <d.nadvoretskiy13@vk.com>
2025-06-19 15:16:20 +0300

    final

---
+++ homework01/task01.java
@@ -0,0 +1,14 @@
+package homework01;
+
+import java.util.Scanner;
+
+public class task01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+        System.out.println(a+"   "+b+"   "+c+"   "+d+"   ");
+    }
+}
--- homework01/task02.java
+++ homework01/task02.java
@@ -1,7 +1,21 @@
 package homework01;
 
+import java.util.Random;
+
 public class task02 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Random rand = new Random();
+        int petya = rand.nextInt(3);
+        int vasya = rand.nextInt(3); // камень-0, ножницы-1, бумага-2.
+        if (vasya == petya) {
+            System.out.print("Ничья!");
+        } else {
+            if ((vasya == 1 && petya == 2) || (vasya == 0 && petya == 1)) {
+                System.out.print("Победил Вася!" + " " + vasya + ">" + petya);
+            } else {
+                System.out.print("Победил Петя!" + " " + petya + ">" + vasya);
+
+            }
+        }
     }
 }

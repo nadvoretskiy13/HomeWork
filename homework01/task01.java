ea6c2d473a8fb12321fae90058f414ed0cdc1a77
Overlord <d.nadvoretskiy13@vk.com>
2025-06-19 15:55:26 +0300

    final01

--- homework01/task01.java
+++ homework01/task01.java
@@ -1,14 +1,14 @@
 package homework01;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class task01 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        int d = scan.nextInt();
+        Random rand = new Random();
+        int a = rand.nextInt(100);
+        int b = rand.nextInt(100);
+        int c = rand.nextInt(100);
+        int d = rand.nextInt(100);
         System.out.println(a+"   "+b+"   "+c+"   "+d+"   ");
     }
 }

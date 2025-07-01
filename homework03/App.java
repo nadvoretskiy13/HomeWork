fa0f796ff0533dd059fc4713973ed7b762ee04f5
Overlord <d.nadvoretskiy13@vk.com>
2025-07-01 15:42:31 +0300

    onWork

--- homework03/App.java
+++ homework03/App.java
@@ -1,17 +1,35 @@
 package homework03;
 
-// Реализовать класс Телевизор. У класса есть поля, свойства и методы.
-//Проверить работу в классе App, методе main.
-//Обратить внимание на переопределение метода toString.
+/* Реализовать класс Телевизор. У класса есть поля, свойства и методы.
+Проверить работу в классе App, методе main.
+Обратить внимание на переопределение метода toString.
+Ожидаемый результат:
+1. Создан класс Телевизор;
+2. У класса есть поля, свойства и методы. Поля желательно сделать private.
+ Задать новые значения полям класса можно через конструктор и setters.
+3. В классе переопределен метод toString.
+4. Создан класс App с методом main
+5. В методе main класса App создано несколько экземпляров класса
+Телевизор и проверено, как распечатываются заполненные данные об
+экземплярах класса.
+6. Дополнительно. Задавать параметры класса Телевизор с клавиатуры
+или случайным числом. */
 
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         // конструктор по-умолчанию
-        Television myTelevision = new Television();
-        myTelevision.productionDate = LocalDate.now();
+        Television myTelevision = new Television("Philips",70 ,"3840x2160",2025);
         System.out.println(myTelevision);
+
+        Television sTelevision = new Television("Samsung",65,"3840x2160");
+        System.out.println(sTelevision);
+
+        Television xTelevision = new Television("Xiaomi", 55);
+        System.out.println(xTelevision);
+
     }
 }
--- homework03/Television.java
+++ homework03/Television.java
@@ -1,11 +1,39 @@
 package homework03;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Television {
-    // Поля (свойства) класса
+    Scanner scan = new Scanner(System.in);
     private String model;
     private int screenSize;
-    public LocalDate productionDate;
-}
+    private String screenResolution;
+    private int productionYear;
+
+    public Television(String model, int screenSize, String screenResolution, int productionYear) {
+        this.model = model;
+        this.screenSize = screenSize;
+        this.screenResolution = screenResolution;
+        this.productionYear = productionYear;
+    }
+    public Television(String model, int screenSize, String screenResolution) {
+        this.model = model;
+        this.screenSize = screenSize;
+        this.screenResolution = screenResolution;
+    }
+    public Television(String model, int screenSize) {
+        this.model = model;
+        this.screenSize = screenSize;
+    }
+    public String toString() {
+        return "Television{" +
+                "model=" + model +
+                ", screenSize" + screenSize +
+                ", screenResolution" + screenResolution +
+                ", productionYear" + productionYear +
+                '}';
+        }
+
+    }
+
 

fe172ad352cb76dc55c48ffe27faa6cced959816
Overlord <d.nadvoretskiy13@vk.com>
2025-07-03 13:46:26 +0300

    final

---
+++ homework04/task3.java
@@ -0,0 +1,14 @@
+package homework04;
+
+import java.util.Scanner;
+
+public class task3 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Solution solution = new Solution();
+        String input = scan.nextLine();
+        String sortedAndLowercased = solution.sortWordsAndLowercase(input);
+        System.out.println(sortedAndLowercased);
+    }
+}

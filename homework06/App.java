29ff47a19e06fde8746bb5594780bdcb50ac78f0
Nadvoretskiy Dmitriy <d.nadvoretskiy13@vk.com>
2025-07-15 09:47:35 +0300

    inWork1

--- homework06/App.java
+++ homework06/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
 
 
 

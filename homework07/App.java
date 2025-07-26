f33163b72fb97bb3d75921102d4f467c5df92502
Nadvoretskiy Dmitriy <d.nadvoretskiy13@vk.com>
2025-07-26 16:52:17 +0300

    Final

--- homework07/App.java
+++ homework07/App.java
@@ -1,6 +1,29 @@
 package homework07;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите данные о покупателях: ");
+        String[] personsArray = scanner.nextLine().split(";");
+        DiscountProduct[] discount = new DiscountProduct[][discountArray.length];
+        for (int i = 0; i < personsArray.length; i++) {
+            DiscountProduct p = new DiscountProduct(discountArray[i]);
+            discounts[i] = p;
+        }
+
+        System.out.println("Введите данные о продуктах: ");
+
+        String[] productArray = scanner.nextLine().split(";");
+        Product[] productss = new Product[productArray.length];
+        for (int i = 0; i < productArray.length; i++) {
+            Product pp = new Product(productArray[i]);
+            productss[i] = pp;
+        }
+
+        List<String> purchaseLog = new ArrayList<>();
     }
 }

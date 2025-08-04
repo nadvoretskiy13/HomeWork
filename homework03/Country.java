accc9f499a0dc88e0f5cf8705a7b3fdcc1dac87a
Nadvoretskiy Dmitriy <d.nadvoretskiy13@vk.com>
2025-08-04 13:10:32 +0300

    Create Country.java

---
+++ homework03/Country.java
@@ -0,0 +1,20 @@
+package homework03;
+
+public class Country {
+    private String madeIn;
+    public Country(String madeIn) {
+        this.madeIn = madeIn;
+    }
+    public String getMadeIn() {
+        return madeIn;
+    }
+    public void setMadeIn(String madeIn) {
+        this.madeIn = madeIn;
+    }
+    @Override
+    public String toString() {
+        return "in {" +
+                " " + madeIn + '\'' +
+                "}";
+    }
+}

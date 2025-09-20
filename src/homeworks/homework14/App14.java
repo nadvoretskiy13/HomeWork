0f1de7bd2bb8100f4585f91103161c57b0039a92
Nadvoretskiy Dmitriy <d.nadvoretskiy13@vk.com>
2025-09-20 23:41:37 +0300

    final

--- src/homeworks/homework14/App14.java
+++
@@ -1,7 +0,0 @@
-package homeworks.homework14;
-
-public class App14 {
-    public static void main(String[] args) {
-        System.out.println("Программа запускается, но тестирование выполняется через JUnit.");
-    }
-}
